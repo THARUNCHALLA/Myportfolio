dbcbfe4bdab7841821d463eaceb2a932cabdcc2a
THARUN CHALLA <challatharun29@gmail.com>
2023-12-27 13:34:48 +0530

    first

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import { useRef } from 'react';
+import React, { useState,useRef } from 'react'
+import {useForm} from "react-hook-form"
 import emailjs from '@emailjs/browser';
 import "./Form.css"
 const Form = () => {
+  console.log(useForm())
+  const [state12,setState]=useState(true)
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
+    setState(false)
     emailjs.sendForm('service_y5gsfif', 'template_10on7eh', form.current, 'VmWPAmTZzYZAR3gKD')
       .then((result) => {
           console.log(result.text);
@@ -16,7 +19,7 @@ const Form = () => {
 
   return (
     <div className='form'>
-        <form ref={form} onSubmit={sendEmail}>
+       {state12?<form ref={form} onSubmit={sendEmail}>
             <label>Your Name</label>
             <input type="text"  name="user_name" required/>
             <label>Email</label>
@@ -26,7 +29,9 @@ const Form = () => {
             <label>Message</label>
             <textarea rows="6" cols="50" placeholder='Type Your Message Here' name='message'/>
             <button className='ViewButton1'>Submit</button>
-        </form>
+        </form>: <p className='Thankyou'>Thank You for message &#128522;
+    </p>
+}
     </div>
   )
 }
