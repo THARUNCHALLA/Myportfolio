92c9202f765b187923e771f06a615b5221d6033c
CHALLA THARUN <139211590+THARUNCHALLA@users.noreply.github.com>
2024-01-04 22:33:43 +0530

    Update Hero.js

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -10,7 +10,6 @@ const Hero = () => {
         </div>
         <div className='banner-text'>
             <p className='p'>I'm Tharun Challa</p>
-            <h1 className='h1'>Front-End Developer</h1>
             <div className='social'>
             <a href="https://www.linkedin.com/in/tharun-challa-a59739248/" target='_blank' rel="noreferrer"><FaLinkedin size={22} style={{ color: "white", marginRight: "1rem" }} /></a>
                             <a href='https://www.facebook.com/stories/113635216498345/UzpfSVNDOjExMDg5Mjg0MDAzMDQ4MTM=/?bucket_count=9&source=story_tray' target='_blank' rel="noreferrer"><FaFacebook size={22} style={{ color: "white", marginRight: "1rem" }} /></a>
@@ -19,7 +18,7 @@ const Hero = () => {
                         </div>
             <div className='ButtonContainer'>
                 <Link to="/Project"><button className='button1'>Project</button></Link>
-                <Link to="/Contact"><button className='button2'>Contact</button></Link>
+               <button className='button2'>LinkedIn</button>
             </div>
         </div>
     </div>
